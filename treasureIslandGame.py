97cfcb7bcb2d99095b107cee79d6daf60849f801
Kathelyn Euzebio <kathelynjacob3@gmail.com>
2025-06-21 15:20:42 -0300

    first commit

---
+++ treasureIslandGame.py
@@ -0,0 +1,47 @@
+print('''
+*******************************************************************************
+          |                   |                  |                     |
+ _________|________________.=""_;=.______________|_____________________|_______
+|                   |  ,-"_,=""     `"=.|                  |
+|___________________|__"=._o`"-._        `"=.______________|___________________
+          |                `"=._o`"=._      _`"=._                     |
+ _________|_____________________:=._o "=._."_.-="'"=.__________________|_______
+|                   |    __.--" , ; `"=._o." ,-"""-._ ".   |
+|___________________|_._"  ,. .` ` `` ,  `"-._"-._   ". '__|___________________
+          |           |o`"=._` , "` `; .". ,  "-._"-._; ;              |
+ _________|___________| ;`-.o`"=._; ." ` '`."\` . "-._ /_______________|_______
+|                   | |o;    `"-.o`"=._``  '` " ,__.--o;   |
+|___________________|_| ;     (#) `-.o `"=.`_.--"_o.-; ;___|___________________
+____/______/______/___|o;._    "      `".o|o_.--"    ;o;____/______/______/____
+/______/______/______/_"=._o--._        ; | ;        ; ;/______/______/______/_
+____/______/______/______/__"=._o--._   ;o|o;     _._;o;____/______/______/____
+/______/______/______/______/____"=._o._; | ;_.--"o.--"_/______/______/______/_
+____/______/______/______/______/_____"=.o|o_.--""___/______/______/______/____
+/______/______/______/______/______/______/______/______/______/______/_____ /
+*******************************************************************************
+''')
+print("Welcome to Treasure Island.")
+print("Your mission is to find the treasure.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
+direction = int(input("Which direction do you want to go <- ->?\n1 - Left\n2 - Right?\n"))
+if(direction != 1):
+          print("You fall into a hole.\nGAME OVER!!!")
+else:
+          print("Do you want to go swimming 🏊‍♂️ or wait 🙇‍♀️?")
+          action = int(input("1 - swim\n2 - wait\n"))
+          if action != 2:
+                    print("Attacked by trout.\nGAME OVER!!!")
+          else:
+                    print("Choose a door 🚪:")
+                    door= int(input("1 - Red\n2 - Blue\n3 - Yellow\n"))
+                    if(door == 1):
+                              print("Burned by fire.\nGAME OVER!!!")
+                    elif(door == 2):
+                              print("Eaten by beasts.\nGAME OVER!!!")
+                    elif(door == 3):
+                              print("YOU WIN!!!")
+                    else:
+                              print("GAME OVER!!!")
